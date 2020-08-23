370f6e5af472ad01462ff21a9d329faa9f8a058e
alejo <jhonalejoo@gmail.com>
2020-08-22 21:15:45 -0500

    drawer

--- src/redux/eps/actions.js
+++ src/redux/eps/actions.js
@@ -1,15 +1,15 @@
 import {GET_EPS_ERROR, GET_EPS_REQUESTING, GET_EPS_SUCCESS} from "./constant";
 
-export const getEpsRequesting = () => {
+export const getEpsRequesting = () => ({
     type: GET_EPS_REQUESTING
-};
+});
 
-export const getEpsSuccess = (eps) => {
+export const getEpsSuccess = (eps) => ({
     type: GET_EPS_SUCCESS,
     eps
-};
+});
 
-export const getEpsError = (error) => {
+export const getEpsError = (error) => ({
     type: GET_EPS_ERROR,
     error
-};
+});
