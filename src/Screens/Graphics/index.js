ae3a979676663cf48f1d585bf4ab3bc231cac736
alejo <jhonalejoo@gmail.com>
2020-08-23 22:37:17 -0500

    drawer

--- src/Screens/Graphics/index.js
+++ src/Screens/Graphics/index.js
@@ -1,14 +1,69 @@
 import React,{useEffect,useState,} from 'react';
 import {useDispatch,useSelector} from "react-redux";
 import {View,Dimensions,StyleSheet} from 'react-native';
-import { TabView, SceneMap } from 'react-native-tab-view';
+import { TabView,TabBar,SceneMap } from 'react-native-tab-view';
+import Theme from "../../Theme/Theme";
+import {heightPercentageToDP as hp} from "react-native-responsive-screen";
+import {
+    LineChart,
+} from "react-native-chart-kit";
+
+const data = {
+    labels: ["Marzo", "Abril", "Mayo", "Junio","Julio","Agosto"],
+    datasets: [
+        {
+            data: [0, 0, 0, 80, 99, 186],
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+            strokeWidth: 1.5
+        }
+    ],
+    legend: ["Curva contagio"]
+};
+
+
+const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130d",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 250, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+};
+
+const screenWidth = Dimensions.get("window").width;
+const GraphComponent =()=>{
+    return(
+        <LineChart
+            data={data}
+            width={screenWidth}
+            height={hp(40)}
+            verticalLabelRotation={30}
+            chartConfig={chartConfig}
+            bezier
+        />
+    )
+};
 
 const FirstRoute = () => (
-    <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+    <View style={[styles.scene,{backgroundColor:'#43307a',alignItems:'center',justifyContent:'center'}]} >
+        <GraphComponent/>
+    </View>
 );
 
 const SecondRoute = () => (
-    <View style={[{flex:1}, { backgroundColor: '#673ab7' }]} />
+    <View style={[styles.scene,{backgroundColor:'#43307a',alignItems:'center',justifyContent:'center'}]} >
+        <GraphComponent/>
+    </View>
+);
+
+const renderTabBar = props => (
+    <TabBar
+        {...props}
+        indicatorStyle={{ backgroundColor: 'white' }}
+        style={{ backgroundColor: Theme.COLORS.PRIMARY }}
+    />
 );
 
 
@@ -19,8 +74,8 @@ const GraphicsScreen = ({navigation}) => {
     const [index, setIndex] = useState(0);
 
     const [routes] = useState([
-        { key: 'first', title: 'First' },
-        { key: 'second', title: 'Second' },
+        { key: 'first', title: 'Contagio' },
+        { key: 'second', title: 'Ucis' },
     ]);
 
     const renderScene = SceneMap({
@@ -30,6 +85,8 @@ const GraphicsScreen = ({navigation}) => {
 
     return (
         <TabView
+            indicatorStyle={{backgroundColor:'white'}}
+            renderTabBar={renderTabBar}
             navigationState={{ index, routes }}
             renderScene={renderScene}
             onIndexChange={setIndex}
--- src/Screens/statistics/index.js
+++ src/Screens/statistics/index.js
@@ -1,34 +1,12 @@
 import React,{useEffect,useState,} from 'react';
 import {useDispatch,useSelector} from "react-redux";
 import {View} from 'react-native';
-import styles from "./styles";
-import { LinearGradient, Stop, Defs } from 'react-native-svg'
-import { BarChart, Grid } from 'react-native-svg-charts'
 import HeaderComponent from '../../Components/Header';
 import Theme from '../../Theme/Theme';
 import ButtonStatics from '../../Components/Buttons/buttonStatics';
 import {Text,ButtonGroup} from 'react-native-elements';
-import dataCovid from "../../redux/dataCovid/reducer";
 
 const StatisticsScreen = ({navigation}) => {
-
-    const data    = [ 50, 10, 40, 95, 4, 24, 85, 91, 35, 53, 53, 24, 50, 20, 80 ];
-    const buttons = ['Total', 'Hoy', 'Ayer'];
-    const [index,setIndex] = useState(0);
-    const AllData= useSelector(state=>state. dataCovid);
-    const covidData=AllData.data;
-    const updateIndex =(index)=>{
-        setIndex(index)
-    };
-
-    const Gradient = () => (
-        <Defs key={'gradient'}>
-            <LinearGradient id={'gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
-                <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'}/>
-                <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'}/>
-            </LinearGradient>
-        </Defs>
-    );
 
     return(
         <View style={{flex:1,backgroundColor:Theme.COLORS.PRIMARY,height:'100%'}}>
@@ -51,26 +29,6 @@ const StatisticsScreen = ({navigation}) => {
                     <ButtonStatics width={'47%'} number={1} statistic='Nuevos Infectados' color={Theme.COLORS.BLUE} />
                 </View>
             </View>
-           {/* <View style={{
-                height:'35%',
-                backgroundColor:Theme.COLORS.WHITE,
-                borderTopLeftRadius:30,
-                borderTopRightRadius: 30,
-                padding: 15
-            }}>
-                <BarChart
-                    style={ { height: 200 } }
-                    data={ data }
-                    contentInset={ { top: 20, bottom: 20 } }
-                    svg={ {
-                        strokeWidth: 2,
-                        fill: 'url(#gradient)',
-                    } }
-                >
-                    <Grid/>
-                    <Gradient/>
-                </BarChart>
-            </View>*/}
         </View>
     )
 };
